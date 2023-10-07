fc83f69f794930f8be96fcbe95c45e5a90f9e3d9
nilesh23 <nkatkar23@gmail.com>
2023-10-07 18:59:31 +0530

    Assignment 2 uploaded

---
+++ Assignment2/src/Question_3.java
@@ -0,0 +1,18 @@
+
+
+
+/*
+ * Q3. Create a class called Date that includes three fields—a month (type int), a day (type int) and a
+year (type int). Provide a constructor that initializes the three instance variables and assumes that
+the values provided are correct. Provide a set and a get method for each instance
+variable. Provide a method displayDate that displays the month, day and year separated by forward
+slashes (/). Write a test application named DateTest that demonstrates class Date’s capabilities.
+ */
+public class Question_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Assignment2/src/Question_4.java
@@ -0,0 +1,31 @@
+
+
+/*
+ * Q4. Create a class Point2D , in package - "com.app.geometry" : for representing a point in x-y co-
+ordinate system. Create a parameterized constructor to init x & y co-ords. Add a method to return
+string form of point's x & y co-ords
+
+Hint : public String getDetails())
+Add isEqual method to Point2D class :a boolean returning method : must return true if n only if
+both points are having same x,y co-ords or false otherwise.
+Add calculateDistance method to calculate distance between current point and specified point &
+return the distance to the caller.
+
+Hint : Use distance formula . Use java.lang.Math class methods --sqrt, pow etc.
+Write TestPoint class , in package "tester" , with a main method, Accept co ordinates of 2 points
+from user (Scanner) --to create 2 points (p1 & p2)
+
+Use getDetails method to display point details.(p1's details & p2's details)
+
+Invoke isEqual & display if points are same or different (i.e p1 & p2 are located at the same
+position)
+If they are not located at the same position , display distance between p1 & p2
+ */
+public class Question_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
