51fd8af33424625f96b4e292ce399347825ac1e9
nilesh23 <nkatkar23@gmail.com>
2023-10-17 23:21:47 +0530

    Assignment 6 Question 2 uploaded

---
+++ Assignment6/Assignment6_2/src/CircleTester.java
@@ -0,0 +1,19 @@
+import com.circle.Circle;
+import com.circle.NegativeDiameterException;
+
+public class CircleTester {
+	public static void main(String[] args) {
+		Circle c1=new Circle();
+		System.out.println(c1);
+		
+		try {
+			Circle c2=new Circle(4,4,-2);
+			System.out.println(c2);
+		}	
+		catch(NegativeDiameterException n)
+		{
+			System.err.println(n);
+		}
+
+	}
+}
---
+++ Assignment6/Assignment6_2/src/com/circle/NegativeDiameterException.java
@@ -0,0 +1,18 @@
+package com.circle;
+
+public class NegativeDiameterException extends RuntimeException{
+//	String message;
+	
+	public NegativeDiameterException() 
+	{
+	}
+	public NegativeDiameterException(String message) 
+	{
+		super(message);
+	}
+
+	@Override
+	public String toString() {
+		return "NegativeDiameterException : " + getMessage() ;
+	}
+}
