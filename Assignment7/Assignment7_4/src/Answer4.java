7b5a62c9c81fbe4306ed4930ff17869859c320ce
nilesh23 <nkatkar23@gmail.com>
2023-10-17 20:13:22 +0530

    Assignment 7 Question 4 uploaded

---
+++ Assignment7/Assignment7_4/src/Answer4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Answer4 {
+		public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+
+	        System.out.print("Enter a string: ");
+	        String input = sc.nextLine();
+
+	        // Remove leading and trailing spaces and split the string by spaces
+	        String[] words = input.trim().split("\\s+");
+
+	        int wordCount = words.length;
+
+	        System.out.println("Number of words: " + wordCount);
+
+	}
+}
---
+++ Assignment7/Assignment7_4/src/Question4.java
@@ -0,0 +1,5 @@
+/*
+ * 
+Q4. write a java program to count number of words in a String.
+Hint: You can use , trim() , length() and split() methods
+ */
