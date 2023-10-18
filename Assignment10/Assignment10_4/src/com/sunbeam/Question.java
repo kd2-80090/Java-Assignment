cfe13d5e146234f97957b935e246c80d9ccaad1b
nilesh23 <nkatkar23@gmail.com>
2023-10-18 15:07:24 +0530

    Assignment 10 Question 4 uploaded

--- Assignment10/Assignment10_4/src/com/sunbeam/Question.java
+++ Assignment10/Assignment10_4/src/com/sunbeam/Question.java
@@ -9,10 +9,21 @@ import java.util.TreeSet;
 public class Question {
 
 	public static void main(String[] args) {
-//		Collection<String> c = new ArrayList<>();
-//		Collection<String> c = new HashSet<>();
+//		Collection<String> c = new ArrayList<>();		
+		//ArrayList--//Elements -> we get in order of we have added 
+		
+//		Collection<String> c = new HashSet<>();			
+		//HashSet--//duplicate values not allowed
+		//HashSet--//null value should be first 
+		//HashSet--//Elements are in sorted order
+
 //		Collection<String> c = new LinkedHashSet<>();
+		//LinkedHashSet --//duplicate values not allowed
+		//LinkedHashSet --//Elements -> we get in order of we have added
+		
 		Collection<String> c = new TreeSet<>();
+		//TreeSet--//It gives NullPointerException if we have assigned any element to null
+		//TreeSet--//Elements->we get it in sorted order
 		c.add("B");
 		c.add("D");
 		c.add("A");
