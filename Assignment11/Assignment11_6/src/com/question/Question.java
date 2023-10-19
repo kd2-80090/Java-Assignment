43792bedad49132092f367d3544ceed42087f22b
nilesh23 <nkatkar23@gmail.com>
2023-10-19 23:48:29 +0530

    Assignment 11 Question 6 updated

---
+++ Assignment11/Assignment11_6/src/com/question/Question.java
@@ -0,0 +1,20 @@
+
+
+
+/*
+6. Store Students in LinkedHashMap<> so that, for given roll, 
+	Student can be searched in fastest possible time. 
+	Do we need to write equals() and
+	hashCode() in Student class? 
+	Follow menu-driven approach. Hint:
+	class Student {
+	// ...
+	}
+	Map<Integer, Student> map = new HashMap<>();
+	s = new Student();
+	acceptStudent(s); // implement method in Main class
+	map.put(s.getRoll(), s);
+	roll = sc.nextInt();
+	s = map.get(roll);
+	
+*/
