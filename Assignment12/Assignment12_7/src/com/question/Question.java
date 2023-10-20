b4a0adba196eca26488cc8074659a7ca3343bb40
nilesh23 <nkatkar23@gmail.com>
2023-10-20 18:36:28 +0530

    Assignment 12 Question 7 uploaded

---
+++ Assignment12/Assignment12_7/src/com/question/Question.java
@@ -0,0 +1,5 @@
+
+
+/*
+7. Write a program to calculate sum of 10 random integers using streams.
+ **/
---
+++ Assignment12/Assignment12_7/src/com/test/TesterFactorialStream.java
@@ -0,0 +1,21 @@
+package com.test;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class TesterFactorialStream {
+	
+	public static void main(String[] args) {
+		Random r = new Random();
+		Stream<Integer> strm  = Stream.generate(() -> r.nextInt(10)).limit(10);
+		List<Integer>randomNumbers = strm.collect(Collectors.toList());
+		randomNumbers.forEach(ele->System.out.println("Random Numbers : " +ele));
+		
+		int sum =  randomNumbers.stream().reduce(0, (a,e)->a+e);
+		System.out.println("Sum of 10 random numbers = "+sum);		
+		
+//		int sum = strm.reduce(0, (a,e) -> a+e);
+//		System.out.println("Sum = "+sum);
+	}
+}
