8e268eeb0c84d9a18a82ce140a92a7017aa5e5a1
nilesh23 <nkatkar23@gmail.com>
2023-10-21 17:44:23 +0530

    Assignment 13 Question 2 uploaded

---
+++ Assignment13/Assignment13_2/src/com/objectoutputstream/book/Book.java
@@ -0,0 +1,73 @@
+package com.objectoutputstream.book;
+
+import java.io.Serializable;
+import java.util.Scanner;
+
+public class Book implements Serializable{
+	public static final long serialVersionUID =1;
+	String isbn;
+	double price;
+	String authorName;
+	int quantity;
+	public Book() {
+		this("",0,"",0);
+	}
+	public Book(String isbn, double price, String authorName, int quantity) {
+		this.isbn = isbn;
+		this.price = price;
+		this.authorName = authorName;
+		this.quantity = quantity;
+	}
+	public void accept() {
+		Scanner sc  =  new Scanner(System.in);
+		System.out.println("Enter unique book id = ");
+		this.isbn = sc.next();
+		System.out.println("Enter price of book = ");
+		this.price = sc.nextDouble();
+		System.out.println("Enter author name = ");
+		this.authorName = sc.next();
+		System.out.println("Enter quantity = ");
+		this.quantity = sc.nextInt();
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getAuthorName() {
+		return authorName;
+	}
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	@Override
+	public String toString() {
+		return "StoreBookList [isbn=" + isbn + ", price=" + price + ", authorName=" + authorName + ", quantity="
+				+ quantity + "]";
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof Book))
+			return false;
+		Book other = (Book) obj;
+		if(this.isbn.equals(other.isbn))
+			return true;
+		else 
+			return false;
+	}	
+}
---
+++ Assignment13/Assignment13_2/src/com/question/Question.java
@@ -0,0 +1,7 @@
+
+
+/*
+2. In menu-driven program of Books (with ArrayList - Day12 Q1) add two more menus. 
+	Save books in ﬁle and load books from ﬁle. 
+	Use ObjectOutputStream	and ObjectInputStream.
+*/
